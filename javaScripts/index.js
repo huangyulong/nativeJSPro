c23336df39b5cc92cd3b06c7efceeb7776d12139
GTR <need_for_speed_9@163.com>
2018-08-22 01:33:42 +0800

    Initial commit

---
+++ javaScripts/index.js
@@ -0,0 +1,34 @@
+let timer = null;
+window.onload = function () {
+    let topAnimation = document.getElementById('topAnimation');
+    let topP = topAnimation.children;
+    let num = 0;
+   
+    topP[0].setAttribute('class','index-top-p-animation')
+    num ++;
+    timer = setTimeout(function(){
+        if(num === 0){
+            topP[topP.length-1].removeAttribute('class');
+            topP[num].setAttribute('class','index-top-p-animation');
+        }else if(num === topP.length){
+            num = 0;
+            topP[topP.length-1].removeAttribute('class');
+            topP[num].setAttribute('class','index-top-p-animation');
+        }else{
+            topP[num-1].removeAttribute('class');
+            topP[num].setAttribute('class','index-top-p-animation');
+        }
+        num++;
+        setTimeout(arguments.callee, 5000)
+        
+    },5000)
+    
+    
+}
+
+window.onunload = function(){
+    clearTimeout(timer)
+}
+
+
+
